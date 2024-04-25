731e0678f38e1493bcbb10d9b2e5d0d79ca5e4ac
davidwolfpaw <david@davidwolfpaw.com>
2024-04-25 10:47:44 -0400

    adds activate and deactivate functions

--- pawsome-portfolio.php
+++ pawsome-portfolio.php
@@ -26,3 +26,25 @@ require_once plugin_dir_path( __FILE__ ) . 'lib/register-post-types.php';
 require_once plugin_dir_path( __FILE__ ) . 'lib/register-taxonomies.php';
 require_once plugin_dir_path( __FILE__ ) . 'lib/register-blocks.php';
 require_once plugin_dir_path( __FILE__ ) . 'lib/render-blocks.php';
+
+/**
+ * Setup plugin on activation
+ */
+function pawsome_activate() {
+	// Register post types and taxonomies
+	create_pawsome_item_post_type();
+	create_pawsome_tag_taxonomy();
+	create_pawsome_category_taxonomy();
+
+	// Flush rewrite rules to ensure the new rules for CPTs and taxonomies are recognized
+	flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'pawsome_activate' );
+
+/**
+ * Cleanup on deactivation
+ */
+function pawsome_deactivate() {
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'pawsome_deactivate' );
