b9a59d23721bbfdf60c9aebc49b13a5022da0f57
davidwolfpaw <david@davidwolfpaw.com>
2025-06-15 19:14:26 -0400

    updates css selector

--- build/portfolio/index.asset.php
+++ build/portfolio/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '191eaee7f6e53dc7f0e6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0f1b79005b16e0b37151');
--- build/portfolio/view.js
+++ build/portfolio/view.js
@@ -1,1 +1,154 @@
-document.addEventListener("DOMContentLoaded",(function(){const t=document.querySelectorAll(".pawsome-portfolio-item"),e=document.querySelectorAll(".pawsome-portfolio-tag-button"),n=document.createElement("button"),a=new URLSearchParams(window.location.search);let o=a.get("tags")?a.get("tags").split(",").map(Number):[];function s(){o.length>0?t.forEach((t=>{const e=JSON.parse(t.getAttribute("data-tag-ids")).some((t=>o.includes(t)));t.style.display=e?"":"none"})):t.forEach((t=>t.style.display=""))}if(n.textContent="Clear Tags",n.className="clear-tags-button",e.length>0&&e[e.length-1].parentNode.appendChild(n),e.forEach((t=>{t.addEventListener("click",(function(){const t=parseInt(this.getAttribute("data-tag-id")),e=o.indexOf(t);e>-1?(o.splice(e,1),this.classList.remove("active")):(o.push(t),this.classList.add("active")),s()}))})),n.addEventListener("click",(function(){o=[],e.forEach((t=>t.classList.remove("active"))),t.forEach((t=>t.style.display=""))})),e.forEach((t=>{t.addEventListener("click",(function(){const t=parseInt(this.dataset.tagId),e=new URLSearchParams(window.location.search);let n=e.get("tags")?e.get("tags").split(",").map(Number):[];const a=n.indexOf(t);a>-1?n.splice(a,1):n.push(t),e.set("tags",n.join(",")),window.history.replaceState({},"",`${window.location.pathname}?${e}`)}))})),o.forEach((t=>{const e=document.querySelector(`.pawsome-portfolio-tag-button[data-tag-id="${t}"]`);e&&e.classList.add("active"),s()})),"modal"===document.querySelector(".pawsome-portfolio").getAttribute("data-link-behavior")){const i=document.getElementById("pawsome-modal-container"),d=i.querySelector(".close");let r;function c(){i.style.display="none",r&&r.focus()}t.forEach((t=>{t.addEventListener("click",(function(){const t=this.dataset.postId;fetch(`/wp-json/wp/v2/pawsome_item/${t}`).then((t=>t.json())).then((t=>{!async function(t){r=document.activeElement;const e=i.querySelector(".pawsome-modal-content");e.innerHTML=`\n\t            <h2>${t.title.rendered}</h2>\n\t            ${t.featured_image_src?`<img src="${t.featured_image_src}" alt="${t.title.rendered}">`:""}\n\t            ${t.content.rendered}\n\t        `,i.style.display="flex",e.focus()}(t)}))}))})),d.addEventListener("click",c),document.addEventListener("keydown",(function(t){"Escape"===t.key&&c()})),i.addEventListener("click",(function(t){t.target===i&&c()}))}}));
+/******/ (() => { // webpackBootstrap
+/*!*******************************!*\
+  !*** ./src/portfolio/view.js ***!
+  \*******************************/
+/**
+ * Use this file for JavaScript code that you want to run in the front-end
+ * on posts/pages that contain this block.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  const portfolioItems = document.querySelectorAll('.pawsome-portfolio-item');
+  const tagButtons = document.querySelectorAll('.pawsome-portfolio-tag-button');
+  const clearButton = document.createElement('button');
+  const params = new URLSearchParams(window.location.search);
+  let selectedTags = params.get('tags') ? params.get('tags').split(',').map(Number) : [];
+  clearButton.textContent = 'Clear Tags';
+  clearButton.className = 'clear-tags-button';
+
+  // Append the clear button after the last tag button
+  if (tagButtons.length > 0) {
+    tagButtons[tagButtons.length - 1].parentNode.appendChild(clearButton);
+  }
+  tagButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      const tagId = parseInt(this.getAttribute('data-tag-id'));
+      const index = selectedTags.indexOf(tagId);
+      if (index > -1) {
+        // Remove tag from array
+        selectedTags.splice(index, 1);
+        this.classList.remove('active');
+      } else {
+        // Add tag to array
+        selectedTags.push(tagId);
+        this.classList.add('active');
+      }
+      filterPosts();
+    });
+  });
+
+  // Clear all selected tags
+  clearButton.addEventListener('click', function () {
+    selectedTags = [];
+    tagButtons.forEach(button => button.classList.remove('active'));
+    portfolioItems.forEach(item => item.style.display = '');
+  });
+
+  // Update URL with a query string for selected tags
+  tagButtons.forEach(tag => {
+    tag.addEventListener('click', function () {
+      const tagId = parseInt(this.dataset.tagId);
+      const params = new URLSearchParams(window.location.search);
+      let selectedTags = params.get('tags') ? params.get('tags').split(',').map(Number) : [];
+      const index = selectedTags.indexOf(tagId);
+      if (index > -1) {
+        // Remove tag if it's already in the array
+        selectedTags.splice(index, 1);
+      } else {
+        // Add tag to the array
+        selectedTags.push(tagId);
+      }
+
+      // Update the URL, converting array to comma-separated string
+      params.set('tags', selectedTags.join(','));
+      window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
+    });
+  });
+
+  // Ensures that tags in query string are filtered on page load
+  selectedTags.forEach(tagId => {
+    const tagElement = document.querySelector(`.pawsome-portfolio-tag-button[data-tag-id="${tagId}"]`);
+    if (tagElement) {
+      tagElement.classList.add('active');
+    }
+    filterPosts();
+  });
+
+  // Filter posts based on selected tags
+  function filterPosts() {
+    if (selectedTags.length > 0) {
+      portfolioItems.forEach(item => {
+        const tags = JSON.parse(item.getAttribute('data-tag-ids'));
+        const isMatch = tags.some(tag => selectedTags.includes(tag));
+        item.style.display = isMatch ? '' : 'none';
+      });
+    } else {
+      portfolioItems.forEach(item => item.style.display = '');
+    }
+  }
+
+  // Build modal lightbox to display portfolio item content
+  const portfolioContainer = document.querySelector('.pawsome-portfolio');
+  const linkBehavior = portfolioContainer.getAttribute('data-link-behavior');
+
+  // Only if the link behavior has been set to modal
+  if (linkBehavior === 'modal') {
+    const modal = document.getElementById('pawsome-modal-container');
+    const closeButton = modal.querySelector('.close');
+
+    // Add click event listeners to each portfolio item
+    portfolioItems.forEach(item => {
+      item.addEventListener('click', function () {
+        const postId = this.dataset.postId;
+        fetch(`/wp-json/wp/v2/pawsome_item/${postId}`).then(response => response.json()).then(post => {
+          showModal(post);
+        });
+      });
+    });
+    let lastFocusedElement;
+
+    // Function to open the modal
+    async function showModal(post) {
+      lastFocusedElement = document.activeElement; // Save the currently focused element
+      const modalContent = modal.querySelector('.pawsome-modal-content');
+      modalContent.innerHTML = `
+	            <h2>${post.title.rendered}</h2>
+	            ${post.featured_image_src ? `<img src="${post.featured_image_src}" alt="${post.title.rendered}">` : ''}
+	            ${post.content.rendered}
+	        `;
+      modal.style.display = 'flex';
+      modalContent.focus();
+    }
+
+    // Function to close the modal
+    function closeModal() {
+      modal.style.display = 'none';
+      if (lastFocusedElement) {
+        // Return focus to the element that opened the modal
+        lastFocusedElement.focus();
+      }
+    }
+
+    // Event listener for close button
+    closeButton.addEventListener('click', closeModal);
+
+    // Event listener for escape key
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    });
+
+    // Event listener for clicking anywhere outside of the modal content
+    modal.addEventListener('click', function (event) {
+      if (event.target === modal) {
+        closeModal();
+      }
+    });
+  }
+});
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
